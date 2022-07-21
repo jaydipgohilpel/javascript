0e9ef5eee6332ba93af18884746f857c1f685897
PE86PD\PEPD <jaydip.gohil@proexelancers.com>
2022-07-21 15:06:14 +0530

    zz

--- task-2/index.js
+++ task-2/index.js
@@ -5,115 +5,145 @@ function select() {
     var value = select.options[select.selectedIndex].value;
 
 
-    console.log(mainArray);
-    // console.log(mainArray.length-1);
-    // console.log(mainArray[mainArray.length - 1].length);
+// Create an "li" node:
+const node = document.createElement("li");
 
-    for (let i = 0; i < le; i++) {
-        // if (mainArray.length===1) {
-        //     mainArray[i].push(value);
-        //     break;
-        // }
-        for (let j = mainArray[i].length - 1; j < mainArray[i].length; j++) {
+// Create a text node:
+const textnode = document.createTextNode("Water");
 
-            if (mainArray[i][j] !== value) {
-                console.log("selected", mainArray[i][j]);
-                console.log("value", value);
-                display();
-                break;
-                mainArray.push(value);
-            }
-            else {
-                mainArray[i].push(value);
-                display();
-                // break;
-            }
-            // if (!mainArray[i][j] == value) {
-            //     console.log("selected", mainArray[i][j]);
-            //     console.log("value", value);
-            //     mainArray[i].push(value);
-            // }
+// Append the text node to the "li" node:
+node.appendChild(textnode);
+
+// Append the "li" node to the list:
+document.getElementById("myList").appendChild(node);
+
+
+    
 
 
 
-            //     if (!mainArray[i][j] == value) {
-            //     mainArray[i].push(value);
-            //     break;
-            // }
-        }
-        console.log("mainArray", mainArray[i]);
-        // if (mainArray !== value) {
-        //     console.log("selected", mainArray[i][j]);
-        //     console.log("value", value);
-        //     mainArray.push(value);
-        //     display();
-        //     break;
-
-        // }
-        // else {
-        //     display();
-        //     continue;
-        //     // break;
-        // }
 
 
 
-        // if (!mainArray[i][mainArray[i].length] == value) {
-        //     mainArray[i].push(value);
-        //     break;
-        // }
-        // mainArray[i].push(value);
-        // break;
-    }
-    return false;
+    // let t = "";
+    // let first = "";
+    // first = mainArray[0][0];
+    // for (let i = 0; i < mainArray.length; i++) {
+
+    //     for (let j = mainArray[i].length - 1; j < mainArray[i].length; j++) {
+    //         if (mainArray[i][j] === value) {
+    //             break;
+    //         }    
+    //         if (mainArray[i][j] !== value) {               
+    //             mainArray[i].push(value);              
+    //             display();
+    //             return false;
+    //         }
+    //     }
+    // }
+    // mainArray.push([value]);
+    // display();
+    // return false;
 }
+
 
 function display() {
 
     let t = "";
+    let first = "";
+    let second = "";
     for (let i = 0; i < mainArray.length; i++) {
+        first = mainArray[0][0];
+        second = mainArray[0][1];
         for (let j = 0; j < mainArray[i].length; j++) {
-            t += mainArray[i][j] + " ";
+            if (i == 0) {
+                t += mainArray[i][j];
+            }
+            if (i > 0) {
+                if (mainArray[i - 1][j] === mainArray[i][j]) {
+                    t += mainArray[i][j];
+                    continue;
+                }
+                if (mainArray[i - 1][j] !== mainArray[i][j]) {
+                    // t += "&nbsp;&nbsp;" + mainArray[i][j];
+                    t += "" + mainArray[i][j];
+                    continue;
+                }
+            }
+
         }
         t += "<br>";
         document.getElementById("output").innerHTML = t;
     }
-
-}
-//  window.location.reload();
-function reset() {
-    document.getElementById("bookAddForm").reset();
 }
 
+// mainArray[0].push("1");
+// mainArray[0].push("2");
+// mainArray[0].push("3");
+// mainArray[0].push("f");
+
+// mainArray.push(["4"]);
+// mainArray.push("5");
+// mainArray.push("6");
+// mainArray.push("7");
+// mainArray[1].push("1");
+// mainArray[1].push("2");
+// mainArray[1].push("3");
+// mainArray[1].push("f");
 
 
 
 
-mainArray[0].push("1");
-mainArray[0].push("2");
-mainArray[0].push("3");
-mainArray[0].push("f");
 
-mainArray.push(["4"]);
-mainArray[1].push("1");
-mainArray[1].push("2");
-mainArray[1].push("3");
-mainArray[1].push("f");
-mainArray.push("5");
-mainArray.push("6");
-mainArray.push("7");
 
-console.log(mainArray);
-console.log(mainArray[0].length - 1);
-console.log(mainArray[0][0]);
-console.log(mainArray.length - 1);
-console.log(mainArray[mainArray.length - 1].length);
-let t = "";
-for (let i = 0; i < mainArray.length; i++) {
-    for (let j = 0; j < mainArray[i].length; j++) {
-        t += mainArray[i][j];
-    }
-    t += "<br>";
-}
-document.write(t);
+// console.log(mainArray);
+// console.log(mainArray[0].length - 1);
+// console.log(mainArray[0][0]);
+// console.log(mainArray.length - 1);
+// console.log(mainArray[mainArray.length - 1].length);
+// let t = "";
+// for (let i = 0; i < mainArray.length; i++) {
+//     for (let j = 0; j < mainArray[i].length; j++) {
+//         t += mainArray[i][j];
+//     }
+//     t += "<br>";
+// }
+// document.write(t);
+// // document.write("m==>"+mainArray[4][mainArray[4].length - 1]);
 
+// var table = document.getElementById("myTable");
+// var row = table.insertRow(0);
+// var cell1 = row.insertCell(0);
+// var cell2 = row.insertCell(1);
+// var cell2 = row.insertCell(2);
+// cell1.innerHTML = "h";
+// cell2.innerHTML = "t";
+
+
+
+//main code add
+
+// for (let j = mainArray[i].length - 1; j < mainArray[i].length; j++) {
+
+//     if (mainArray[i][j] === value) {
+//         break;
+//     }
+//     if (mainArray[i][j] !== value) {
+//         mainArray[i].push(value);
+//         display();
+//         return false;
+//     }
+  
+
+
+// }
+
+
+// }
+
+// mainArray.push([value]);
+// // document.getElementById("output").innerHTML = t;
+// display();
+// return false;
+
+                                                                                                                    
